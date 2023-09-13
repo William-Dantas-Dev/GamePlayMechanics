578a12b0648ceca83397c242aae7c07afd5055ee
William-Dantas-Dev <95831281+William-Dantas-Dev@users.noreply.github.com>
2023-09-13 19:15:39 -0300

    Follow The Player

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3f;
+    private Rigidbody rb;
+    private GameObject player;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        rb.AddForce(lookDirection * speed);
+    }
+}
